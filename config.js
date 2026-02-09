0fa2dc9951527b2d8ca2aed43f507912c4719d3b
llcgs-admin <dlhopmann@Gmail.com>
2026-02-09 14:18:08 -0600

    Update config.js with actual Google deployment ID

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const API_BASE = "https://script.google.com/macros/s/DEPLOYMENT_ID/exec";
+export const API_BASE = "https://script.google.com/macros/s/AKfycbzwkAaezFhRf1BsVd_6UmbP6H4FBfXKlljMrLHLyKQHoOn_qVsBcDRyrWgZofCI9HtX8w/exec";
